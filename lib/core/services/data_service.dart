ca3fc42fd9269e62f2efebe866b561b47af3528e
Karim Motaz <karemmotaz2929@gmail.com>
2025-07-04 15:02:40 +0300

    add firebaseto the project

---
+++ lib/core/services/data_service.dart
@@ -0,0 +1,18 @@
+
+abstract class DatabaseService {
+  // Future<void> addData({
+  //   required String path,
+  //   required Map<String, dynamic> data,
+  //   String? documentId,
+  // });
+
+  Future<dynamic> getData({
+    required String path,
+    String? documentId,
+  });  
+
+  Future<bool> checkIfDataExists({
+    required String path,
+    required String documentId,
+  });
+}
