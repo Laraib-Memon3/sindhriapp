4bc36620ae9249e7c4cd13d35d64538d7fcf03dc
Laraib Memon <laraibmemon.bssef22@iba-suk.edu.pk>
2025-07-04 23:38:24 +0500

    solve errors

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sindhri/view/splash_screen.dart';
 
+import 'view/splash_screen.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
