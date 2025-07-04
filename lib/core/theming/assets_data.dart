c1eaf608d63eb8296c21186ebb5863c2b749a4f1
Karim Motaz <karemmotaz2929@gmail.com>
2025-07-04 11:34:01 +0300

    add assets folder and make assets data class

---
+++ lib/core/theming/assets_data.dart
@@ -0,0 +1,34 @@
+class AssetsData {
+  static const String customizeYourBurger =
+      'assets/Images/Customize_Your_Burger/Customize_Your_Burger.png';
+  static const String masterCard = 'assets/Images/Payment/Master_Card.png';
+  static const String profileIcon = 'assets/Images/Payment/Visa.png';
+  static const String visa = 'assets/Images/profile/Profile_Image.png';
+  static const String splashBurgerOne =
+      'assets/Images/splash/Splash_Burger_One.png';
+  static const String splashBurgerTwo =
+      'assets/Images/splash/Splash_Burger_Two.png';
+  static const String bacons = 'assets/Images/toppings/Bacons.png';
+  static const String coleslaw = 'assets/Images/toppings/Coleslaw.png';
+  static const String fries = 'assets/Images/toppings/Fries.png';
+  static const String onion = 'assets/Images/toppings/Onion.png';
+  static const String onions = 'assets/Images/toppings/Onions.png';
+  static const String pickles = 'assets/Images/toppings/Pickles.png';
+  static const String salad = 'assets/Images/toppings/Salad.png';
+  static const String tomato = 'assets/Images/toppings/Tomato.png';
+  static const String comment = 'assets/SVG/home/comment.svg';
+  static const String heartIcon = 'assets/SVG/home/heart.svg';
+  static const String homeIcon = 'assets/SVG/home/home.svg';
+  static const String searchIcon = 'assets/SVG/home/search.svg';
+  static const String settingsSlidersIcon =
+      'assets/SVG/home/settings-sliders.svg';
+  static const String userIcon = 'assets/SVG/home/user.svg';
+  static const String arrowIcon = 'assets/SVG/profile/arrow.svg';
+  static const String editIcon = 'assets/SVG/profile/edit.svg';
+  static const String lockIcon = 'assets/SVG/profile/lock.svg';
+  static const String settingsIcon = 'assets/SVG/profile/settings.svg';
+  static const String signOutIcon = 'assets/SVG/profile/sign-out.svg';
+  static const String logo = 'assets/SVG/splash/logo.svg';
+  static const String addIcon = 'assets/SVG/add.svg';
+  static const String removeIcon = 'assets/SVG/remove.svg';
+}
