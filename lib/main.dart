563f30404f11350171ea5c52dbb831f49ebe13b0
Laraib Memon <laraibmemon.bssef22@iba-suk.edu.pk>
2025-07-05 12:13:38 +0500

    added login screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-import 'package:sindhri/view/Product1.dart';
-import 'package:sindhri/view/home_screen.dart';
-=======
 import 'package:sindhri/view/splash_screen.dart';
->>>>>>> 049f59042032a7b9882b5231bebee34de95eb6da
-
-import 'view/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -32,11 +25,7 @@ class MainApp extends StatelessWidget {
       ),
       title: 'Sindhri',
       debugShowCheckedModeBanner: false,
-<<<<<<< HEAD
-      home: Product1(),
-=======
       home: SplashScreen(),
->>>>>>> 049f59042032a7b9882b5231bebee34de95eb6da
     );
   }
 }
